8c22198857a4b2c6a016321ec9245b9b13c95143
Deepblue <deepblue.xwm@gmail.com>
2019-04-14 22:54:07 +0800

    Add runtime permission application and SPUtil.

--- app/src/main/java/com/xwm/androidproject/base/BaseActivity.java
+++ app/src/main/java/com/xwm/androidproject/base/BaseActivity.java
@@ -3,18 +3,28 @@ package com.xwm.androidproject.base;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
+import com.xwm.androidproject.callback.PermissionListener;
+
 import org.greenrobot.eventbus.EventBus;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Created by Adam on 2018-10-26
  */
 @SuppressLint("Registered")
 public class BaseActivity extends AppCompatActivity {
+    private PermissionListener mListener;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -77,6 +87,52 @@ public class BaseActivity extends AppCompatActivity {
         mStartActivityTag = tag;
         mStartActivityTime = SystemClock.uptimeMillis();
         return result;
+    }
+
+    /**
+     * 检查和处理运行时权限，并将用户授权的结果通过PermissionListener进行回调。
+     *
+     * @param permissions 要检查和处理的运行时权限数组
+     * @param listener    用于接收授权结果的监听器
+     */
+    protected void handlePermissions(List<String> permissions, PermissionListener listener) {
+        if (permissions == null) {
+            return;
+        }
+        mListener = listener;
+        List<String> requestPermissionList = new ArrayList<>();
+        for (String permission : permissions) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissionList.add(permission);
+            }
+        }
+        if (!requestPermissionList.isEmpty()) {
+            ActivityCompat.requestPermissions(this, requestPermissionList.toArray(new String[]{}), 1);
+        } else {
+            listener.onGranted();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 0) {
+                List<String> deniedPermissions = new ArrayList<>();
+                for (int i = 0; i < grantResults.length; i++) {
+                    int grantResult = grantResults[i];
+                    String permission = permissions[i];
+                    if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                        deniedPermissions.add(permission);
+                    }
+                }
+                if (deniedPermissions.isEmpty()) {
+                    mListener.onGranted();
+                } else {
+                    mListener.onDenied(deniedPermissions);
+                }
+            }
+        }
     }
 
     @Override
