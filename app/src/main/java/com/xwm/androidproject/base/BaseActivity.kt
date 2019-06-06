7eb21c690f16388e7b5c75120d91bfeb715afd44
Deepblue <deepblue.xwm@gmail.com>
2019-06-06 17:14:51 +0800

    Remove permission processing code

--- app/src/main/java/com/xwm/androidproject/base/BaseActivity.kt
+++ app/src/main/java/com/xwm/androidproject/base/BaseActivity.kt
@@ -2,22 +2,16 @@ package com.xwm.androidproject.base
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.SystemClock
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
-import com.xwm.androidproject.callback.PermissionListener
 import org.greenrobot.eventbus.EventBus
-import java.util.*
 
 /**
  * @author Created by Adam on 2018-10-26
  */
 @SuppressLint("Registered")
 open class BaseActivity : RxAppCompatActivity() {
-    private lateinit var mListener: PermissionListener
 
     private var mStartActivityTag: String? = null
     private var mStartActivityTime: Long = 0
@@ -76,51 +70,6 @@ open class BaseActivity : RxAppCompatActivity() {
         mStartActivityTag = tag
         mStartActivityTime = SystemClock.uptimeMillis()
         return result
-    }
-
-    /**
-     * 检查和处理运行时权限，并将用户授权的结果通过PermissionListener进行回调。
-     *
-     * @param permissions 要检查和处理的运行时权限数组
-     * @param listener    用于接收授权结果的监听器
-     */
-    protected fun handlePermissions(permissions: List<String>?, listener: PermissionListener) {
-        if (permissions == null) {
-            return
-        }
-        mListener = listener
-        val requestPermissionList = ArrayList<String>()
-        for (permission in permissions) {
-            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-                requestPermissionList.add(permission)
-            }
-        }
-        if (!requestPermissionList.isEmpty()) {
-            ActivityCompat.requestPermissions(this, requestPermissionList.toTypedArray(), 1)
-        } else {
-            listener.onGranted()
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 1) {
-            if (grantResults.size > 0) {
-                val deniedPermissions = ArrayList<String>()
-                for (i in grantResults.indices) {
-                    val grantResult = grantResults[i]
-                    val permission = permissions[i]
-                    if (grantResult != PackageManager.PERMISSION_GRANTED) {
-                        deniedPermissions.add(permission)
-                    }
-                }
-                if (deniedPermissions.isEmpty()) {
-                    mListener.onGranted()
-                } else {
-                    mListener.onDenied(deniedPermissions)
-                }
-            }
-        }
     }
 
     override fun onDestroy() {
